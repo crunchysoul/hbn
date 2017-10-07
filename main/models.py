8c1e5a53c21335c5793554949a576d0042da4d79
andrew-m-h <andrew.m.h96@gmail.com>
2017-10-07 06:24:21 +0000

    add models

--- main/models.py
+++ main/models.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from markupfield.fields import MarkupField
 
 class Recipe (models.Model):
+    title = models.CharField(max_length=128);
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    date_published = models.DateTiemField(default=timezone.now)
+    date_published = models.DateTimeField(default=timezone.now)
     method = MarkupField(default_markup_type='markdown')
 
-class Rateing (models.Model):
-    value = PositiveSmallIntegerField()
-    comment = TextField()
+class Rating (models.Model):
+    value = models.PositiveSmallIntegerField(null=True)
+    comment = models.TextField()
     recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
 
